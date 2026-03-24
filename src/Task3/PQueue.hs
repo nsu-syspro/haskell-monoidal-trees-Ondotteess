b71ca5f32f9512890aa8d3cb80577d7b449ecc2a
Ondotteess <osipovichivan99@gmail.com>
2026-03-24 23:41:38 +0700

    task 3

--- src/Task3/PQueue.hs
+++ src/Task3/PQueue.hs
@@ -3,9 +3,12 @@
 
 module Task3.PQueue where
 
+import Common.MonoidalTree
 import Common.PriorityQueue
 
-import Task1 (Measured(..), MinMax(..))
+import Data.Foldable (toList)
+
+import Task1 (Measured(..), Min(..), Max(..), MinMax(..))
 import Task3.Tree
 
 -- * Priority queue definition
@@ -14,20 +17,184 @@ import Task3.Tree
 newtype PQueue k v = PQueue { getTree :: Tree (MinMax k) (Entry k v) }
   deriving (Show, Eq)
 
-
 -- | Priority queue entry wrapper
 newtype Entry k v = Entry { getEntry :: (k, v) }
   deriving (Show, Eq)
 
 instance Ord k => Measured (MinMax k) (Entry k v) where
-  measure = error "TODO: define measure (Measured (MinMax k) (Task3.Entry k v))"
+  measure (Entry (k, _)) = measure k
 
 -- * Priority queue instance
 
 instance PriorityQueue PQueue where
-  empty = error "TODO: define empty (PriorityQueue Task3.PQueue)"
-  toPriorityQueue = error "TODO: define toPriorityQueue (PriorityQueue Task3.PQueue)"
-  entries = error "TODO: define entries (PriorityQueue Task3.PQueue)"
-  insert = error "TODO: define insert (PriorityQueue Task3.PQueue)"
-  extractMin = error "TODO: define extractMin (PriorityQueue Task3.PQueue)"
-  extractMax = error "TODO: define extractMax (PriorityQueue Task3.PQueue)"
+  empty = PQueue Empty
+
+  toPriorityQueue = PQueue . toTree . map Entry . toList
+
+  entries (PQueue t) = foldMap (\(Entry kv) -> [kv]) t
+
+  insert k v (PQueue t) = PQueue (t |> Entry (k, v))
+
+  extractMin (PQueue t) = do
+    (v, res) <- extractMinTree t
+    pure (v, PQueue (normalizeRemove res))
+
+  extractMax (PQueue t) = do
+    (v, res) <- extractMaxTree t
+    pure (v, PQueue (normalizeRemove res))
+
+minKey :: Ord k => Tree (MinMax k) (Entry k v) -> Maybe k
+minKey t =
+  case measure t of
+    MinMax (PosInf, _) -> Nothing
+    MinMax (Min k, _)  -> Just k
+
+maxKey :: Ord k => Tree (MinMax k) (Entry k v) -> Maybe k
+maxKey t =
+  case measure t of
+    MinMax (_, NegInf) -> Nothing
+    MinMax (_, Max k)  -> Just k
+
+extractMinTree
+  :: Ord k
+  => Tree (MinMax k) (Entry k v)
+  -> Maybe (v, RemoveResult (MinMax k) (Entry k v))
+extractMinTree Empty = Nothing
+extractMinTree (Leaf (Entry (_, v))) = Just (v, Short Empty)
+extractMinTree (Node2 _ l r)
+  | chooseLeftMin l r =
+      do
+        (v, res) <- extractMinTree l
+        pure (v, case res of
+          Removed l' -> Removed (node2 l' r)
+          Short l'   -> fixNode2Left l' r)
+  | otherwise =
+      do
+        (v, res) <- extractMinTree r
+        pure (v, case res of
+          Removed r' -> Removed (node2 l r')
+          Short r'   -> fixNode2Right l r')
+extractMinTree (Node3 _ l m r)
+  | chooseLeftMin3 l m r =
+      do
+        (v, res) <- extractMinTree l
+        pure (v, case res of
+          Removed l' -> Removed (node3 l' m r)
+          Short l'   -> fixNode3Left l' m r)
+  | chooseMiddleMin3 l m r =
+      do
+        (v, res) <- extractMinTree m
+        pure (v, case res of
+          Removed m' -> Removed (node3 l m' r)
+          Short m'   -> fixNode3Middle l m' r)
+  | otherwise =
+      do
+        (v, res) <- extractMinTree r
+        pure (v, case res of
+          Removed r' -> Removed (node3 l m r')
+          Short r'   -> fixNode3Right l m r')
+
+extractMaxTree
+  :: Ord k
+  => Tree (MinMax k) (Entry k v)
+  -> Maybe (v, RemoveResult (MinMax k) (Entry k v))
+extractMaxTree Empty = Nothing
+extractMaxTree (Leaf (Entry (_, v))) = Just (v, Short Empty)
+extractMaxTree (Node2 _ l r)
+  | chooseLeftMax l r =
+      do
+        (v, res) <- extractMaxTree l
+        pure (v, case res of
+          Removed l' -> Removed (node2 l' r)
+          Short l'   -> fixNode2Left l' r)
+  | otherwise =
+      do
+        (v, res) <- extractMaxTree r
+        pure (v, case res of
+          Removed r' -> Removed (node2 l r')
+          Short r'   -> fixNode2Right l r')
+extractMaxTree (Node3 _ l m r)
+  | chooseLeftMax3 l m r =
+      do
+        (v, res) <- extractMaxTree l
+        pure (v, case res of
+          Removed l' -> Removed (node3 l' m r)
+          Short l'   -> fixNode3Left l' m r)
+  | chooseMiddleMax3 l m r =
+      do
+        (v, res) <- extractMaxTree m
+        pure (v, case res of
+          Removed m' -> Removed (node3 l m' r)
+          Short m'   -> fixNode3Middle l m' r)
+  | otherwise =
+      do
+        (v, res) <- extractMaxTree r
+        pure (v, case res of
+          Removed r' -> Removed (node3 l m r')
+          Short r'   -> fixNode3Right l m r')
+
+chooseLeftMin :: Ord k => Tree (MinMax k) (Entry k v) -> Tree (MinMax k) (Entry k v) -> Bool
+chooseLeftMin l r =
+  case (minKey l, minKey r) of
+    (Just kl, Just kr) -> kl <= kr
+    (Just _, Nothing)  -> True
+    _                  -> False
+
+chooseLeftMax :: Ord k => Tree (MinMax k) (Entry k v) -> Tree (MinMax k) (Entry k v) -> Bool
+chooseLeftMax l r =
+  case (maxKey l, maxKey r) of
+    (Just kl, Just kr) -> kl >= kr
+    (Just _, Nothing)  -> True
+    _                  -> False
+
+chooseLeftMin3
+  :: Ord k
+  => Tree (MinMax k) (Entry k v)
+  -> Tree (MinMax k) (Entry k v)
+  -> Tree (MinMax k) (Entry k v)
+  -> Bool
+chooseLeftMin3 l m r =
+  case (minKey l, minKey m, minKey r) of
+    (Just kl, Just km, Just kr) -> kl <= km && kl <= kr
+    (Just _, Nothing, Nothing)  -> True
+    (Just kl, Just km, Nothing) -> kl <= km
+    (Just kl, Nothing, Just kr) -> kl <= kr
+    _                           -> False
+
+chooseMiddleMin3
+  :: Ord k
+  => Tree (MinMax k) (Entry k v)
+  -> Tree (MinMax k) (Entry k v)
+  -> Tree (MinMax k) (Entry k v)
+  -> Bool
+chooseMiddleMin3 l m r =
+  case (minKey l, minKey m, minKey r) of
+    (_, Just km, Just kr)       -> maybe True (> km) (minKey l) && km <= kr
+    (Nothing, Just _, Nothing)  -> True
+    _                           -> False
+
+chooseLeftMax3
+  :: Ord k
+  => Tree (MinMax k) (Entry k v)
+  -> Tree (MinMax k) (Entry k v)
+  -> Tree (MinMax k) (Entry k v)
+  -> Bool
+chooseLeftMax3 l m r =
+  case (maxKey l, maxKey m, maxKey r) of
+    (Just kl, Just km, Just kr) -> kl >= km && kl >= kr
+    (Just _, Nothing, Nothing)  -> True
+    (Just kl, Just km, Nothing) -> kl >= km
+    (Just kl, Nothing, Just kr) -> kl >= kr
+    _                           -> False
+
+chooseMiddleMax3
+  :: Ord k
+  => Tree (MinMax k) (Entry k v)
+  -> Tree (MinMax k) (Entry k v)
+  -> Tree (MinMax k) (Entry k v)
+  -> Bool
+chooseMiddleMax3 l m r =
+  case (maxKey l, maxKey m, maxKey r) of
+    (_, Just km, Just kr)       -> maybe True (< km) (maxKey l) && km >= kr
+    (Nothing, Just _, Nothing)  -> True
+    _                           -> False
--- src/Task3/Seq.hs
+++ src/Task3/Seq.hs
@@ -1,9 +1,13 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# OPTIONS_GHC -Wall #-}
 -- The above pragma enables all warnings
 
 module Task3.Seq where
 
+import Common.MonoidalTree
 import Common.Sequence
+
+import Data.Foldable (toList)
 
 import Task1 (Measured(..), Size(..))
 import Task3.Tree
@@ -20,23 +24,120 @@ newtype Elem a = Elem { getElem :: a }
 
 -- | Measures given element as 'Size 1'
 instance Measured (Size a) (Elem a) where
-  measure = error "TODO: define measure (Measured (Size a) (Task3.Elem a))"
+  measure _ = Size 1
 
 instance Foldable Seq where
-  foldMap = error "TODO: define foldMap (Foldable Task3.Seq)"
+  foldMap f (Seq t) = foldMap (f . getElem) t
 
   -- An O(1) implementation of length is possible
   -- due to size of the tree being cached at each node
   length :: forall a. Seq a -> Int
-  length = error "TODO: define length (Foldable Task3.Seq)"
+  length (Seq t) = treeSize t
 
 -- * Sequence instance
 
 instance Sequence Seq where
-  empty = error "TODO: define empty (Sequence Task3.Seq)"
-  toSequence = error "TODO: define toSequence (Sequence Task3.Seq)"
-  (+|) = error "TODO: define (+|) (Sequence Task3.Seq)"
-  (|+) = error "TODO: define (|+) (Sequence Task3.Seq)"
-  insertAt = error "TODO: define insertAt (Sequence Task3.Seq)"
-  removeAt = error "TODO: define removeAt (Sequence Task3.Seq)"
-  elemAt = error "TODO: define elemAt (Sequence Task3.Seq)"
+  empty = Seq Empty
+
+  toSequence = Seq . toTree . map Elem . toList
+
+  x +| Seq t = Seq (Elem x <| t)
+
+  Seq t |+ x = Seq (t |> Elem x)
+
+  insertAt i x (Seq t) = Seq (normalizeInsert (insertAtTree j (Elem x) t))
+    where
+      j = max 0 (min i (treeSize t))
+
+  removeAt i (Seq t)
+    | i < 0 || i >= treeSize t = Seq t
+    | otherwise                = Seq (normalizeRemove (removeAtTree i t))
+
+  elemAt i (Seq t)
+    | i < 0 || i >= treeSize t = Nothing
+    | otherwise                = getElem <$> elemAtTree i t
+
+treeSize :: forall a. Tree (Size a) (Elem a) -> Int
+treeSize t = getSize (measure t :: Size a)
+
+insertAtTree :: Int -> Elem a -> Tree (Size a) (Elem a) -> InsertResult (Size a) (Elem a)
+insertAtTree _ x Empty = Inserted (leaf x)
+insertAtTree i x (Leaf y)
+  | i <= 0    = Overflow (leaf x) (leaf y)
+  | otherwise = Overflow (leaf y) (leaf x)
+insertAtTree i x (Node2 _ l r)
+  | i <= sl =
+      case insertAtTree i x l of
+        Inserted l'    -> Inserted (node2 l' r)
+        Overflow a b   -> Inserted (node3 a b r)
+  | otherwise =
+      case insertAtTree (i - sl) x r of
+        Inserted r'    -> Inserted (node2 l r')
+        Overflow a b   -> Inserted (node3 l a b)
+  where
+    sl = treeSize l
+insertAtTree i x (Node3 _ l m r)
+  | i <= sl =
+      case insertAtTree i x l of
+        Inserted l'    -> Inserted (node3 l' m r)
+        Overflow a b   -> Overflow (node2 a b) (node2 m r)
+  | i <= sl + sm =
+      case insertAtTree (i - sl) x m of
+        Inserted m'    -> Inserted (node3 l m' r)
+        Overflow a b   -> Overflow (node2 l a) (node2 b r)
+  | otherwise =
+      case insertAtTree (i - sl - sm) x r of
+        Inserted r'    -> Inserted (node3 l m r')
+        Overflow a b   -> Overflow (node2 l m) (node2 a b)
+  where
+    sl = treeSize l
+    sm = treeSize m
+
+removeAtTree :: Int -> Tree (Size a) (Elem a) -> RemoveResult (Size a) (Elem a)
+removeAtTree _ Empty = Removed Empty
+removeAtTree _ (Leaf _) = Short Empty
+removeAtTree i (Node2 _ l r)
+  | i < sl =
+      case removeAtTree i l of
+        Removed l' -> Removed (node2 l' r)
+        Short l'   -> fixNode2Left l' r
+  | otherwise =
+      case removeAtTree (i - sl) r of
+        Removed r' -> Removed (node2 l r')
+        Short r'   -> fixNode2Right l r'
+  where
+    sl = treeSize l
+removeAtTree i (Node3 _ l m r)
+  | i < sl =
+      case removeAtTree i l of
+        Removed l' -> Removed (node3 l' m r)
+        Short l'   -> fixNode3Left l' m r
+  | i < sl + sm =
+      case removeAtTree (i - sl) m of
+        Removed m' -> Removed (node3 l m' r)
+        Short m'   -> fixNode3Middle l m' r
+  | otherwise =
+      case removeAtTree (i - sl - sm) r of
+        Removed r' -> Removed (node3 l m r')
+        Short r'   -> fixNode3Right l m r'
+  where
+    sl = treeSize l
+    sm = treeSize m
+
+elemAtTree :: Int -> Tree (Size a) (Elem a) -> Maybe (Elem a)
+elemAtTree _ Empty = Nothing
+elemAtTree i (Leaf x)
+  | i == 0    = Just x
+  | otherwise = Nothing
+elemAtTree i (Node2 _ l r)
+  | i < sl    = elemAtTree i l
+  | otherwise = elemAtTree (i - sl) r
+  where
+    sl = treeSize l
+elemAtTree i (Node3 _ l m r)
+  | i < sl      = elemAtTree i l
+  | i < sl + sm = elemAtTree (i - sl) m
+  | otherwise   = elemAtTree (i - sl - sm) r
+  where
+    sl = treeSize l
+    sm = treeSize m
